d9edb559937631ee80318292a023516757a772e8
vivek3454 <vivekparde2411@gmail.com>
2025-05-06 19:51:55 +0530

    Dashboard layout

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Board!</p>      
+    </div>
+  );
+}
