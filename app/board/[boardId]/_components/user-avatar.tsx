3e23e4d356d11c177e4c04985a4c90591c0f9933
vivek3454 <vivekparde2411@gmail.com>
2025-06-05 16:40:42 +0530

    Room Participants

---
+++ app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,24 @@
+import { Hint } from "@/components/hint";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+interface UserAvatarProps {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  borderColor?: string;
+}
+export function UserAvatar({
+  src,
+  name,
+  fallback,
+  borderColor,
+}: UserAvatarProps) {
+  return (
+    <Hint label={name || "Teammate"} side="bottom" sideOffset={18}>
+      <Avatar className="h-8 w-8 border-2" style={{ borderColor }}>
+        <AvatarImage src={src} />
+        <AvatarFallback className="text-xs font-semibold">{fallback}</AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,27 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#FF5733", // Red-orange
+  "#33C1FF", // Sky blue
+  "#28A745", // Green
+  "#FFC107", // Amber
+  "#6F42C1", // Purple
+  "#E83E8C", // Pink
+  "#17A2B8", // Teal
+  "#343A40", // Dark gray
+  "#007BFF", // Blue
+  "#DC3545", // Red
+  "#20C997", // Mint
+  "#6610F2", // Indigo
+  "#FD7E14", // Orange
+];
+
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId:number):string {
+  return COLORS[connectionId % COLORS.length];
 }
