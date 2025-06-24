b3ed775dadda34868ea528d4ddad47c7d35b36d1
vivek3454 <vivekparde2411@gmail.com>
2025-06-24 14:51:07 +0530

    Selection Net

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { Camera, Color, Point, Side, XYWH } from "@/types/canvas";
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Camera, Color, Layer, Point, Side, XYWH } from "@/types/canvas";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 const COLORS = [
   "#FF5733", // Red-orange
@@ -18,9 +18,8 @@ const COLORS = [
   "#FD7E14", // Orange
 ];
 
-
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function connectionIdToColor(connectionId: number): string {
@@ -32,40 +31,76 @@ export function pointerEventToCanvasPoint(e: React.MouseEvent, camera: Camera) {
 
   return {
     x: Math.round(e.clientX) - camera.x,
-    y: Math.round(e.clientY) - camera.y
+    y: Math.round(e.clientY) - camera.y,
   };
 }
 
 export function colorToCss(color: Color): string {
-  return `#${color.r.toString(16).padStart(2, '0')}${color.g.toString(16).padStart(2, '0')}${color.b.toString(16).padStart(2, '0')}`;
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
 }
 
-export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {  
+export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
   const result = {
     x: bounds.x,
     y: bounds.y,
     width: bounds.width,
-    height: bounds.height
-  }
+    height: bounds.height,
+  };
 
-  if((corner & Side.Left) === Side.Left){
+  if ((corner & Side.Left) === Side.Left) {
     result.x = Math.min(point.x, bounds.x + bounds.width);
     result.width = Math.abs(bounds.x + bounds.width - point.x);
   }
 
-  if((corner & Side.Right) === Side.Right){
+  if ((corner & Side.Right) === Side.Right) {
     result.x = Math.min(point.x, bounds.x);
-    result.width = Math.abs(point.x-bounds.x);
+    result.width = Math.abs(point.x - bounds.x);
   }
 
-  if((corner & Side.Top) === Side.Top){
+  if ((corner & Side.Top) === Side.Top) {
     result.y = Math.min(point.y, bounds.y + bounds.height);
     result.height = Math.abs(bounds.y + bounds.height - point.y);
   }
-  if((corner & Side.Bottom) === Side.Bottom){
+  if ((corner & Side.Bottom) === Side.Bottom) {
     result.y = Math.min(point.y, bounds.y);
     result.height = Math.abs(point.y - bounds.y);
   }
 
   return result;
+}
+
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y),
+  };
+
+  const ids = [];
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, width, height } = layer;
+
+    if (
+      rect.x + rect.width > x &&
+      rect.x < x + width &&
+      rect.y + rect.height > y &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+  return ids;
 }
